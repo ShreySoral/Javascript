6a5036a22916d60f2fdcc687d12d59a9653e33f3
Shrey Soral <78461926+ShreySoral@users.noreply.github.com>
2022-12-24 22:41:38 +0530

    Add files via upload

---
+++ 2.Var_Let_Const.js
@@ -0,0 +1,12 @@
+console.log("var, let and const")
+// var a=45;
+let b="shrey";
+b=4
+const author=5 //Throws an error because constant can't be changed
+var c=null
+var d=undefined;
+{
+    let b="this";
+    console.log(b)
+}
+console.log(b)
