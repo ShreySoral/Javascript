56bc8595f583e11dd75ef1c85753a9e5e0779aad
Shrey Soral <78461926+ShreySoral@users.noreply.github.com>
2022-12-29 12:37:04 +0530

    Add files via upload

---
+++ 13.String_Methods_in_js.js
@@ -0,0 +1,29 @@
+// strings are immutable
+let name = "Shrey"
+console.log(name.length)
+console.log(name.toUpperCase())
+console.log(name.toLowerCase())
+console.log(name.slice(2,4))
+console.log(name.slice(2))
+console.log(name.replace("S","s"))
+let newName = name.trim()   //removes whitespaces
+console.log(newName)
+let Name1 = "Shrey"
+let friend = "John"
+console.log(Name1.concat(" is a friend of ", friend, " Ok"))
+let friend2 = "       meena          "
+console.log(friend2)
+console.log(friend2.trim())
+let country = "INDIA"
+console.log(country[0])
+console.log(country[1])
+console.log(country[2])
+console.log(country[3])
+console.log(country[4])
+console.log(country.toLowerCase())
+let fr = "Raman" + "Shivika" + "Shrey"
+console.log(fr[0])
+console.log(fr[1])
+console.log(fr[2])
+console.log(fr[3])
+// fr[4]= "o" // This is not possible 
