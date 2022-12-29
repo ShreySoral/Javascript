dc5269e4dc9617ba1211ee361b066b0690841f47
Shrey Soral <78461926+ShreySoral@users.noreply.github.com>
2022-12-29 11:10:41 +0530

    Add files via upload

---
+++ 12.Strings_in_js.js
@@ -0,0 +1,19 @@
+let name = "Shrey"
+console.log(name)
+console.log(name[0])
+console.log(name[1])
+console.log(name.length)
+let lastName = 'Soral'
+console.log(lastName)
+
+// template literals
+let boy1 = "pramod"
+let boy2 = "nikhil"
+// nikhil is the friend of pramod
+let sentence = `${boy1} is a friend of ${boy2}`
+console.log(sentence)
+
+// escape sequence charachters
+let fruit = 'Bana\'na'
+console.log(fruit)
+console.log(fruit.length)
